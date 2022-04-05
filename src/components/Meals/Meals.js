db1e91eed55bf4889b9dc5d39604196321517d8d
Roman Lesnoy <romanlesnoy@outlook.com>
2022-04-05 17:05:55 +0300

    Create a Meals component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import MealsSummary from './MealsSummary';
+import MealsList from './MealsList';
+
+const Meals = () => {
+    return (
+        <React.Fragment>
+            <MealsSummary />
+            <MealsList />
+        </React.Fragment>
+    );
+};
+
+export default Meals;
