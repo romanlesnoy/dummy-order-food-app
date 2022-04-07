dbb176e88a1a05a2377356cbc060bfb2768f97f1
Roman Lesnoy <romanlesnoy@outlook.com>
2022-04-07 17:23:31 +0300

    Add the functionality of opening and closing the cart popup

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -13,14 +13,19 @@ const Cart = (props) => {
     );
 
     return (
-        <Modal>
+        <Modal onClose={props.onClose}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>25.62</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes['button--all']}>Close</button>
+                <button
+                    className={classes['button--all']}
+                    onClick={props.onClose}
+                >
+                    Close
+                </button>
                 <button className={classes.button}>Order</button>
             </div>
         </Modal>
